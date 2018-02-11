c0539e7e00e3bcb99127ce83b1d6bd6c582fdc3e
nahayatim <33836117+nahayatim@users.noreply.github.com>
2018-02-11 22:12:42 +0100

    Update register.php

--- cr10/register.php
+++ cr10/register.php
@@ -171,7 +171,7 @@
 <head>
 
   <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="theodora_patkos_styles.css">
+  
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  
